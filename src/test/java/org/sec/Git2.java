86d01fd37f5174489b52943f373d1959e530412a
charan149 <charanram149@gmail.com>
2020-05-01 17:27:36 +0530

    hello

---
+++ src/test/java/org/sec/Git2.java
@@ -0,0 +1,17 @@
+package org.sec;
+
+public class Git2 {
+public void boy() {
+	System.out.println("chennai to salem");
+
+}
+public void girl() {
+	System.out.println("covai to chennai");
+}
+public static void main(String[] args) {
+	Git2 gg=new Git2();
+	gg.boy();
+	gg.girl();
+}
+}	
+
